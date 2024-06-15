cc82c9f4504991557ae5dcbd6f6e0f74a6368a63
Sivakorn Watcharavakhin <88345008+philsvk@users.noreply.github.com>
2024-06-15 16:52:59 +0700

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    awscc = {
+      source = "hashicorp/awscc"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "awscc" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "useast2"
+}
